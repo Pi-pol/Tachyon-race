c7d586cb4ab804302c259404b7fa04bbd09aa846
Jakub Sanecki <115787745+jsan82@users.noreply.github.com>
2024-03-24 00:43:42 +0100

    Added many settings

---
+++ Scenes/masterVolume.gd
@@ -0,0 +1,19 @@
+extends HSlider
+@export 
+var Master : String
+
+var bus_index: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	bus_index = AudioServer.get_bus_index(Master)
+	value_changed.connect(_on_value_changed)
+	
+	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+	
+func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		bus_index,
+		linear_to_db(value)
+	)
+	print(value)

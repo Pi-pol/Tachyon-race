b1c78c1521762840b4addc3b9bc4e5dd6e9e0385
Jakub Sanecki <115787745+jsan82@users.noreply.github.com>
2024-03-23 20:33:13 +0100

    start

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -2,8 +2,16 @@ extends Node
 
 func _ready():
 	var startButton = get_node("GridContainer/StartButton")
-	startButton.pressed.connect(self._on_button_pressed)	
+	startButton.pressed.connect(self._start_button_pressed)	
+	var settingsButton = get_node("GridContainer/SettingsButton")
+	settingsButton.pressed.connect(self._settings_button_pressed)
 	
-func _on_button_pressed():
+func _start_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/1stlevel.tscn")
+	
+func _settings_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
+	
+
+	
 	

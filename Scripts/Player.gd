c03ef6c4935eca3efdcccd6293b288bcc13bcea1
Rajzer <michal.rajzer03@gmail.com>
2024-03-23 22:56:47 +0100

    Tiles updated

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const ACCELERATION = 2000
-const MAX_SPEED =500
+const MAX_SPEED =300
 const FRICTION = 8000
 
 func _physics_process(delta):
@@ -17,7 +17,9 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO,FRICTION*delta)
 	move_and_slide()
 	if get_slide_collision_count() > 0:
-		_check_pushables(velocity)
+		var push_vel = Vector2.ZERO
+		push_vel = input_vector.normalized()*150
+		_check_pushables(push_vel)
 
 func _check_pushables(motion: Vector2) -> void:
 	if abs(motion.x) + abs(motion.y) > 1:

2bb724bf257325e1adcfdfab24ef5f02468b658a
Rajzer <michal.rajzer03@gmail.com>
2024-03-23 23:36:38 +0100

    Added working doors

---
+++ Scripts/door_script.gd
@@ -0,0 +1,23 @@
+extends TileMap
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+		pass
+
+
+func _on_button_game_door_button_pressed():
+	print("Pressed")
+	set_cell(0, Vector2(6, 2), 0, Vector2(0, 2))
+	set_cell(0, Vector2(6, 3), 0, Vector2(0, 3))
+	set_cell(0, Vector2(7, 2), 0, Vector2(1, 2))
+	set_cell(0, Vector2(7, 3), 0, Vector2(1, 3))
+	set_cell(0, Vector2(8, 2), 0, Vector2(2, 2))
+	set_cell(0, Vector2(8, 3), 0, Vector2(2, 3))
+	

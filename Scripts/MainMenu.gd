96b15484d447d03cabc5d26d3c734aadfd15bab5
Rajzer <michal.rajzer03@gmail.com>
2024-03-23 17:34:55 +0100

    Settings update

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,8 +1,8 @@
 extends Node
 
 func _ready():
-	var startButton = get_node("StartButton")
-	startButton.pressed.connect(self._on_button_pressed)
+	var startButton = get_node("GridContainer/StartButton")
+	startButton.pressed.connect(self._on_button_pressed)	
 	
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/1stlevel.tscn")

061ffb4e8778e17c130059a70492c4a79c8840c8
Bartłomiej Szkodny <szkobart@outlook.com>
2024-03-23 20:42:08 +0100

    title added

---
+++ Scripts/Title.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+@onready var animation_intro = $AnimationPlayer
+
+func _ready():
+	animation_intro.play("Fade_in")
+	get_tree().create_timer(3).timeout.connect(fade_out)
+	
+func fade_out():
+	animation_intro.play("fade_out")
+	get_tree().create_timer(3).timeout.connect(start_menu_scene)
+	
+func start_menu_scene():
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	
+	

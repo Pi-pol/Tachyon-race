8da68bee193c0c7614ca13935c6ab00b8f675bf6
Jakub Sanecki <115787745+jsan82@users.noreply.github.com>
2024-03-24 01:46:06 +0100

    Resolution option

--- Scripts/settingsMenu.gd
+++ Scripts/settingsMenu.gd
@@ -1,9 +1,14 @@
 extends Control
 
+@onready var drop_down_menu = $containers/resolutionsOptionButton
 
+func add_items():
+	drop_down_menu.add_item("1280x720")
+	drop_down_menu.add_item("1600x900")
+	drop_down_menu.add_item("1920x1080")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	add_items()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,3 +26,14 @@ func _on_check_button_toggled(toggled_on):
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
+
+
+func _on_resolutions_option_button_item_selected(index):
+	var current_selected = index
+	
+	if current_selected == 0:
+		get_window().size = Vector2i(1280,720)
+	elif current_selected == 1:
+		get_window().size = Vector2i(1600,900)
+	elif current_selected == 2:
+		get_window().size = Vector2i(1920,1080)

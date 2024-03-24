059bcc369d912f03228c7a5856ed651ecd2c2e47
Rajzer <michal.rajzer03@gmail.com>
2024-03-24 10:38:12 +0100

    yup

--- Scripts/ButtonGame.gd
+++ Scripts/ButtonGame.gd
@@ -3,11 +3,8 @@ extends Node2D
 signal door_keycard_reader_pressed
 
 func _on_action_fild_area_entered(area):
-	var player = get_node("TileMap/Player")
-	if player.inventory = "keycard":
-		$AudioStreamPlayer2D.play()
-		await get_tree().create_timer(0.7).timeout
-		door_button_pressed.emit()
-		queue_free()
-	else:
-		pass
+	
+	$AudioStreamPlayer2D.play()
+	await get_tree().create_timer(0.7).timeout
+	door_keycard_reader_pressed.emit()
+	

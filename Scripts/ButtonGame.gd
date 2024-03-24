8762ab32a53eb401788a5c3ca8d5a9976a7004af
Rajzer <michal.rajzer03@gmail.com>
2024-03-24 11:06:48 +0100

    fixed button

--- Scripts/ButtonGame.gd
+++ Scripts/ButtonGame.gd
@@ -1,10 +1,9 @@
 extends Node2D
 
-signal door_keycard_reader_pressed
+signal door_button_pressed
 
 func _on_action_fild_area_entered(area):
-	
 	$AudioStreamPlayer2D.play()
 	await get_tree().create_timer(0.7).timeout
-	door_keycard_reader_pressed.emit()
-	
+	door_button_pressed.emit()
+	queue_free()

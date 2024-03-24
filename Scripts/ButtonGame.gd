eb5cca84453da63ab5ef0f71725d1be5cb9f110b
Bartłomiej Szkodny <szkobart@outlook.com>
2024-03-24 06:02:09 +0100

    added sounds

--- Scripts/ButtonGame.gd
+++ Scripts/ButtonGame.gd
@@ -3,5 +3,7 @@ extends Node2D
 signal door_button_pressed
 
 func _on_action_fild_area_entered(area):
+	$AudioStreamPlayer2D.play()
+	await get_tree().create_timer(0.7).timeout
 	door_button_pressed.emit()
 	queue_free()

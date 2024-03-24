4d01224e2821e913572e92496006bd96d604bd5c
Bartłomiej Szkodny <szkobart@outlook.com>
2024-03-24 03:04:34 +0100

    Player works

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,16 +8,18 @@ const FRICTION = 8000
 @onready var anim_state =animationTree.get("parameters/playback")
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	input_vector = input_vector.normalized()
 	
 	
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/idle/blend_position", input_vector)
 		animationTree.set("parameters/run/blend_position", input_vector)
+		anim_state.travel("run")
+		
 		velocity = velocity.move_toward(input_vector*MAX_SPEED, ACCELERATION*delta)
 	else:
+		anim_state.travel("idle")
 		
 		velocity = velocity.move_toward(Vector2.ZERO,FRICTION*delta)
 	

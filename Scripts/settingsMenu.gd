ebe01512fc9cb1de0a215b74af2ab8f822c37e44
Jakub Sanecki <115787745+jsan82@users.noreply.github.com>
2024-03-24 01:35:50 +0100

    fullscreen added (don't use yet!)

--- Scripts/settingsMenu.gd
+++ Scripts/settingsMenu.gd
@@ -13,3 +13,11 @@ func _process(delta):
 
 func _on_back_to_menu_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+
+
+func _on_check_button_toggled(toggled_on):
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+

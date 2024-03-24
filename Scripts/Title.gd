63a4da2b272ce9a3d900084aef7d0c7e5c18cabb
Jakub Sanecki <115787745+jsan82@users.noreply.github.com>
2024-03-24 04:35:06 +0100

    settings implementation

--- Scripts/Title.gd
+++ Scripts/Title.gd
@@ -3,8 +3,29 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 @onready var animation_intro = $AnimationPlayer
+var settings_file = "settings.json"
+var whole_settings = {"fullscreen": false, "volume": 1, "resolution": 0}
 
 func _ready():
+	var json_as_text = FileAccess.get_file_as_string(settings_file)
+	if json_as_text:
+		whole_settings = JSON.parse_string(json_as_text)
+		
+	if whole_settings["resolution"] == 0:
+		get_window().size = Vector2i(1280,720)
+	elif whole_settings["resolution"] == 1:
+		get_window().size = Vector2i(1600,900)
+	elif whole_settings["resolution"] == 2:
+		get_window().size = Vector2i(1920,1080)
+		
+	if whole_settings["fullscreen"] == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	var volume = linear_to_db(whole_settings["volume"])
+	AudioServer.set_bus_volume_db(0, whole_settings["volume"])
+	AudioServer.set_bus_mute(0, whole_settings["volume"] < 0.05)
+	
 	animation_intro.play("Fade_in")
 	get_tree().create_timer(3).timeout.connect(fade_out)
 	
